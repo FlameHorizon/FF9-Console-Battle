32f19bfbcecca0c2690479636b20ecfaa9436b0a
FlameHorizon <maciejpiotrowsk@gmail.com>
2022-12-15 08:31:31 +0100

    Implemented left-right cursor movement.

--- FF9.Console/Game.cs
+++ FF9.Console/Game.cs
@@ -4,20 +4,19 @@ public class Game
 {
     private int _cursorPositionLeft = 1;
     private int _cursorPositionTop = 1;
-    
+
     public void Start()
     {
         System.Console.CursorVisible = false;
-        
-        System.Console.WriteLine("-----------");
-        System.Console.WriteLine("|>Attack  |");
-        System.Console.WriteLine("-----------");
-        System.Console.WriteLine("| Steal   |");
-        System.Console.WriteLine("-----------");
-        System.Console.WriteLine("| Item    |");
-        System.Console.WriteLine("-----------");
 
-        
+        System.Console.WriteLine("|---------|---------|");
+        System.Console.WriteLine("|>Attack  | Defend  |");
+        System.Console.WriteLine("|---------|---------|");
+        System.Console.WriteLine("| Steal   |         |");
+        System.Console.WriteLine("|---------|---------|");
+        System.Console.WriteLine("| Item    | Change  |");
+        System.Console.WriteLine("|---------|---------|");
+
         while (true)
         {
             ConsoleKeyInfo k = System.Console.ReadKey(true);
@@ -30,48 +29,94 @@ public class Game
             {
                 // Create down offset.
                 (int left, int top) moveOffset = (0, 2);
-                
+
                 // Check boundaries.
                 if (_cursorPositionTop + moveOffset.top >= 6)
                 {
                     continue;
                 }
-                
+
                 // Clear cursor behind.
-                System.Console.SetCursorPosition(_cursorPositionLeft,_cursorPositionTop);
+                System.Console.SetCursorPosition(_cursorPositionLeft, _cursorPositionTop);
                 System.Console.Write(" ");
 
                 (int left, int top) newCursorPosition =
                     (_cursorPositionLeft + moveOffset.left, _cursorPositionTop + moveOffset.top);
-                
+
                 // Write new cursor location.
-                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top) ;
+                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top);
                 _cursorPositionLeft = newCursorPosition.left;
                 _cursorPositionTop = newCursorPosition.top;
                 System.Console.Write(">");
-                
-                
             }
             else if (k.Key == ConsoleKey.UpArrow)
             {
                 // Create up offset.
                 (int left, int top) moveOffset = (0, -2);
-                
+
                 // Check boundaries.
                 if (_cursorPositionTop + moveOffset.top <= 0)
                 {
                     continue;
                 }
-                
+
                 // Clear cursor behind.
-                System.Console.SetCursorPosition(_cursorPositionLeft,_cursorPositionTop);
+                System.Console.SetCursorPosition(_cursorPositionLeft, _cursorPositionTop);
                 System.Console.Write(" ");
 
                 (int left, int top) newCursorPosition =
                     (_cursorPositionLeft + moveOffset.left, _cursorPositionTop + moveOffset.top);
-                
+
                 // Write new cursor location.
-                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top) ;
+                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top);
+                _cursorPositionLeft = newCursorPosition.left;
+                _cursorPositionTop = newCursorPosition.top;
+                System.Console.Write(">");
+            }
+            else if (k.Key == ConsoleKey.RightArrow)
+            {
+                // Create right offset.
+                (int left, int top) moveOffset = (10, 0);
+
+                // Check boundaries.
+                if (_cursorPositionLeft + moveOffset.left >= 12)
+                {
+                    continue;
+                }
+
+                // Clear cursor behind.
+                System.Console.SetCursorPosition(_cursorPositionLeft, _cursorPositionTop);
+                System.Console.Write(" ");
+
+                (int left, int top) newCursorPosition =
+                    (_cursorPositionLeft + moveOffset.left, _cursorPositionTop + moveOffset.top);
+
+                // Write new cursor location.
+                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top);
+                _cursorPositionLeft = newCursorPosition.left;
+                _cursorPositionTop = newCursorPosition.top;
+                System.Console.Write(">");
+            }
+            else if (k.Key == ConsoleKey.LeftArrow)
+            {
+                // Create left offset.
+                (int left, int top) moveOffset = (-10, 0);
+
+                // Check boundaries.
+                if (_cursorPositionLeft + moveOffset.left < 0)
+                {
+                    continue;
+                }
+
+                // Clear cursor behind.
+                System.Console.SetCursorPosition(_cursorPositionLeft, _cursorPositionTop);
+                System.Console.Write(" ");
+
+                (int left, int top) newCursorPosition =
+                    (_cursorPositionLeft + moveOffset.left, _cursorPositionTop + moveOffset.top);
+
+                // Write new cursor location.
+                System.Console.SetCursorPosition(newCursorPosition.left, newCursorPosition.top);
                 _cursorPositionLeft = newCursorPosition.left;
                 _cursorPositionTop = newCursorPosition.top;
                 System.Console.Write(">");
